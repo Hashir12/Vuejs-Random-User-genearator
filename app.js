eceef270f0a852fa356c5fdc4fba16f3d1aa73b2
Hashir Butt <hashir@ooober.com>
2022-01-05 13:50:42 +0500

    random user application done

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ const app = Vue.createApp({
             gender: 'male',
             pic: 'https://randomuser.me/api/portraits/men/75.jpg'
         }
+    },
+    methods: {
+        async getUser() {
+            const res = await fetch('https://randomuser.me/api')
+            const {results} = await res.json()
+            this.firstName = results[0].name.first
+            this.lastName = results[0].name.last
+            this.email = results[0].email
+            this.gender = results[0].gender
+            this.pic = results[0].picture.large
+        }
     }
 })
 
